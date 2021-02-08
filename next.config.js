7a6591dc9ecac79f3b22df0b40ef9a5a734b81ee
Roman Timashev <roman@tmshv.ru>
2021-02-08 07:06:49 +0300

    add redirect to map

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: 'https://map.latl.ng/SOWQ5LGD4V7GCI3L',
+                permanent: true,
+            },
+        ]
+    }
+}
